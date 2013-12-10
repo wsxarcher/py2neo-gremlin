bc564e47c1be6c9912f90400585a979a93df7728
fabs <fyamagu@gwdg.de>
2013-12-10 16:20:36 +0100

    Added setup.py for convenience.

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2011-2013, Nigel Small
+# Copyright 2011-2013, Fabian Yamaguchi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="py2neo-gremlin",
+    version="0.1",
+    description="Gremlin support for py2neo.",
+    
+    author= "Nigel Small, Fabian Yamaguchi",
+    author_email="nigel@nigelsmall.com, fabs@goesec.de",
+    
+    packages=[
+        "py2neo_gremlin",
+    ],
+    license="Apache License 2.0",
+)
